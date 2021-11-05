afead653e9bbb98e9978080859ff23f1e101fcaf
senayakagunduz <senayakgndz@gmail.com>
2021-11-06 00:35:08 +0300

    an updated commit message

--- src/findPrime.js
+++ src/findPrime.js
@@ -6,23 +6,24 @@ function findPrime(...numbers) {
             console.log( item+ ' is not a prime number')
         else 
             console.log(item +' is prime number')
-    }   
+    } 
+      
 )}
 console.log(perfect(1,2,3,4,10,11))
 /********************************************/
 
 /********************************************/ 
 
-  for(let i=3;i<1000;i++){
+  /*for(let i=3;i<1000;i++){
       for(let j=2;j<i;j++){
           if(i%j==0)
               count+=1
-      }
+        }
       if(count==0){
         console.log(i + " - ")
       }
-      var count=0    
-  }
+         
+  }*/
   
  
         
